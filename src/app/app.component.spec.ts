05b37ec45797089a9e9cc2312c28ec98b4c48ef4
Yury Savin <savinyurii@gmail.com>
2019-05-16 21:17:05 +0300

    Returns single quotes back

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,8 @@
-import { TestBed, async } from "@angular/core/testing";
-import { RouterTestingModule } from "@angular/router/testing";
-import { AppComponent } from "./app.component";
+import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
 
-describe("AppComponent", () => {
+describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [RouterTestingModule],
@@ -10,7 +10,7 @@ describe("AppComponent", () => {
     }).compileComponents();
   }));
 
-  it("should create the app", () => {
+  it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
@@ -19,15 +19,15 @@ describe("AppComponent", () => {
   it(`should have as title 'storeon-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual("storeon-angular");
+    expect(app.title).toEqual('storeon-angular');
   });
 
-  it("should render title in a h1 tag", () => {
+  it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector("h1").textContent).toContain(
-      "Welcome to storeon-angular!"
+    expect(compiled.querySelector('h1').textContent).toContain(
+      'Welcome to storeon-angular!'
     );
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,25 @@
-import { Component, OnInit } from "@angular/core";
-import { NgStoreonService } from "@storeon/angular";
-import { Subject } from "rxjs";
+import { Component, OnInit } from '@angular/core';
+import { NgStoreonService } from '@storeon/angular';
+import { Subject } from 'rxjs';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
   changes: Subject<any>;
   dispatch: Function;
   constructor(private ngstoreon: NgStoreonService) {}
-  title = "storeon-angular";
+  title = 'storeon-angular';
 
   ngOnInit() {
-    const { dispatch, changes } = this.ngstoreon.useStoreon("count");
+    const { dispatch, changes } = this.ngstoreon.useStoreon('count');
     this.dispatch = dispatch;
     this.changes = changes;
   }
 
   updateState() {
-    this.dispatch("inc");
+    this.dispatch('inc');
   }
 }
