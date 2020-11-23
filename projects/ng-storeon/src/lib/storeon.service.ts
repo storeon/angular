2281ecf5b09628017725910fd43e7a4eb9a05660
gladziu90 <gladziu908@gmail.com>
2020-11-23 16:36:56 +0100

    use spaces

--- projects/ng-storeon/src/lib/storeon.service.ts
+++ projects/ng-storeon/src/lib/storeon.service.ts
@@ -6,52 +6,52 @@ import { StoreonEvents, StoreonStore } from 'storeon';
 import { STOREON } from './storeon.token';
 
 @Injectable({
-	providedIn: 'root'
+  providedIn: 'root'
 })
 export class StoreonService<State, Events extends StoreonEvents<State> = any> implements OnDestroy {
 
-	private state$ = new BehaviorSubject<State>(this.store.get());
+  private state$ = new BehaviorSubject<State>(this.store.get());
 
-	private readonly unbind: Function;
+  private readonly unbind: Function;
 
-	constructor(@Inject(STOREON) private store: StoreonStore<State, Events>) {
-		this.unbind = this.store.on('@changed', (state) => {
-			this.state$.next({ ...state as any });
+  constructor(@Inject(STOREON) private store: StoreonStore<State, Events>) {
+    this.unbind = this.store.on('@changed', (state) => {
+      this.state$.next({ ...state as any });
 
-			return null;
-		});
-	}
+      return null;
+    });
+  }
 
-	useStoreon<K>(mapFn: (state: State) => K): Observable<K>;
-	useStoreon<K extends keyof State>(path: K): Observable<State[K]>;
-	useStoreon(pathOrMapFn: ((state: State) => any) | string): Observable<any> {
-		let mapped$;
+  useStoreon<K>(mapFn: (state: State) => K): Observable<K>;
+  useStoreon<K extends keyof State>(path: K): Observable<State[K]>;
+  useStoreon(pathOrMapFn: ((state: State) => any) | string): Observable<any> {
+    let mapped$;
 
-		if (typeof pathOrMapFn === 'string') {
-			mapped$ = this.state$.pipe(pluck(pathOrMapFn));
-		} else if (typeof pathOrMapFn === 'function') {
-			mapped$ = this.state$.pipe(
-				map(source => pathOrMapFn(source))
-			);
-		} else {
-			throw new TypeError(
-				`Unexpected type '${typeof pathOrMapFn}' in select operator,` +
-				` expected 'string' or 'function'`
-			);
-		}
+    if (typeof pathOrMapFn === 'string') {
+      mapped$ = this.state$.pipe(pluck(pathOrMapFn));
+    } else if (typeof pathOrMapFn === 'function') {
+      mapped$ = this.state$.pipe(
+        map(source => pathOrMapFn(source))
+      );
+    } else {
+      throw new TypeError(
+        `Unexpected type '${typeof pathOrMapFn}' in select operator,` +
+        ` expected 'string' or 'function'`
+      );
+    }
 
-		return mapped$.pipe(distinctUntilChanged());
-	}
+    return mapped$.pipe(distinctUntilChanged());
+  }
 
-	dispatch<K extends keyof Events>(event: K, data?: Events[K]) {
-		// @ts-ignore
-		this.store.dispatch(event, data);
-	}
+  dispatch<K extends keyof Events>(event: K, data?: Events[K]) {
+    // @ts-ignore
+    this.store.dispatch(event, data);
+  }
 
-	ngOnDestroy(): void {
-		if (this.unbind) {
-			this.unbind();
-		}
-	}
+  ngOnDestroy(): void {
+    if (this.unbind) {
+      this.unbind();
+    }
+  }
 
 }
